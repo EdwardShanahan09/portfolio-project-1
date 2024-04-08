7b96db1a1a360a68e147ab597fcb30c5f0f3fe2a
Edward Shanahan <edwardshanahan79@gmail.com>
2024-04-08 15:05:54 +0100

    Add about text and images

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,10 +1,37 @@
 import "./About.scss";
+import aboutImg1 from "../../assets/imgs/about-1.jpg";
+import aboutImg2 from "../../assets/imgs/about-2.jpg";
+import aboutImg3 from "../../assets/imgs/about-3.jpg";
+import aboutImg4 from "../../assets/imgs/about-4.jpg";
 
 const About = () => {
+  const aboutImages = [aboutImg1, aboutImg2, aboutImg3, aboutImg4];
   return (
     <section id="about" className="about">
-      <div className="about__img">Image</div>
-      <div className="about__text">Text</div>
+      <div className="about__img">
+        {aboutImages.map((img, index) => {
+          return <img src={img} alt="Ellie Mae" key={index} />;
+        })}
+      </div>
+      <div className="about__text">
+        <h2>A Little About Me</h2>
+
+        <p>
+          Hi, my name is Ellie Mae, and I'm a professional photographer from
+          Clare. I have travelled all around Ireland to shoot weddings, family
+          portraits, events, and festivals.
+        </p>
+
+        <p>
+          I have worked in different agencies, but now I'm freelancing. Since
+          freelancing I have gained hundreds of satisfied clients.
+        </p>
+
+        <p>
+          With my experience and passion for photography, I am very confident
+          you will be happy with my work.
+        </p>
+      </div>
     </section>
   );
 };
