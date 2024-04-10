cdde90e9a6cd238681085a51f9a4f678eb7f8896
Edward Shanahan <edwardshanahan79@gmail.com>
2024-04-10 17:47:07 +0100

    Add testimonials images

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -4,11 +4,25 @@ const Testimonial = (props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const testimonials = props.testimonials;
 
-  console.log(testimonials);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+    }, 9000); // Change slide every 7 seconds
+
+    return () => clearInterval(intervalId);
+  }, [testimonials.length]);
+
   return (
-    <div style={{ background: "pink" }} className="testimonial">
+    <div
+      style={{
+        background: `url(${testimonials[currentIndex].image})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+      className="testimonial"
+    >
       <img
-        src={testimonials[currentIndex].image}
+        src={testimonials[currentIndex].avatar}
         alt={testimonials[currentIndex].name}
       />
       <p>{testimonials[currentIndex].description}</p>
--- src/sections/Testimonials/Testimonials.jsx
+++ src/sections/Testimonials/Testimonials.jsx
@@ -1,31 +1,39 @@
 import "./Testimonials.scss";
 import Testimonial from "../../components/Testimonial/Testimonial";
+import testimonialImage1 from "../../assets/imgs/testimonial-1.jpg";
+import testimonialImage2 from "../../assets/imgs/testimonial-2.jpg";
+import testimonialImage3 from "../../assets/imgs/testimonial-3.jpg";
+import testimonialImage4 from "../../assets/imgs/testimonial-4.jpg";
+import avatarImage1 from "../../assets/imgs/avatar-1.jpg";
+import avatarImage2 from "../../assets/imgs/avatar-2.jpg";
+import avatarImage3 from "../../assets/imgs/avatar-3.jpg";
+import avatarImage4 from "../../assets/imgs/avatar-4.jpg";
 const Testimonials = () => {
   const testimonials = [
     {
-      image: "",
-      avatar: "",
+      image: testimonialImage1,
+      avatar: avatarImage1,
       description:
         "Ellie Mae Photography exceeded my expectations! Ellie's attention to detail and ability to capture genuine emotions in her photos truly sets her apart. I couldn't be happier with the beautiful portraits she created for my family.",
       name: "- Sarah W.",
     },
     {
-      image: "",
-      avatar: "",
+      image: testimonialImage2,
+      avatar: avatarImage2,
       description:
-        "Ellie's talent behind the lens is remarkable. She has a unique ability to make you feel comfortable and natural, resulting in the most authentic and beautiful photos. I'm grateful to have had Ellie capture such precious moments for my family.",
+        "Working with Ellie was an absolute pleasure. Her professionalism and creativity made the entire process seamless. The images she produced were stunning and exceeded all my hopes. I highly recommend Ellie Mae Photography to anyone looking for top-notch quality and service.",
       name: "- Mike H.",
     },
     {
-      image: "",
-      avatar: "",
+      image: testimonialImage3,
+      avatar: avatarImage3,
       description:
-        "Ellie's talent behind the lens is remarkable. She has a unique ability to make you feel comfortable and natural, resulting in the most authentic and beautiful photos. I'm grateful to have had Ellie capture such precious moments for my family.",
+        "Ellie's talent behind the lens is remarkable. She has a unique ability to make you feel comfortable and natural, resulting in the most authentic and beautiful photos. I'm grateful to have had Ellie capture such precious moments for my family",
       name: "- Emily L.",
     },
     {
-      image: "",
-      avatar: "",
+      image: testimonialImage4,
+      avatar: avatarImage4,
       description:
         "Ellie Mae Photography made our special day even more memorable. From the initial consultation to the final delivery of our wedding photos, Ellie's professionalism and passion for her craft were evident. Her ability to capture the essence of our love story is something we'll cherish forever.",
       name: "- Jack and Lauren D.",
