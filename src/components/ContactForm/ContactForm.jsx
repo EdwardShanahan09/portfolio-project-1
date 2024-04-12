752d39b1076555db26d36d0c9cf2101b69b6a232
Edward Shanahan <edwardshanahan79@gmail.com>
2024-04-12 17:16:39 +0100

    Add contact form elements

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,31 @@
+import Button from "../Button/Button";
+
 const ContactForm = () => {
-  return <div className="ContactForm">Contact Form</div>;
+  return (
+    <form className="contact-form">
+      <div className="contact__group">
+        <label htmlFor="name">Name</label>
+        <input name="name" type="text" className="contact__control" />
+      </div>
+
+      <div className="contact__group">
+        <label htmlFor="email">Email</label>
+        <input name="email" type="email" className="contact__control" />
+      </div>
+
+      <div className="contact__group">
+        <label htmlFor="subject">Subject</label>
+        <input name="subject" type="text" className="contact__control" />
+      </div>
+
+      <div className="contact__group">
+        <label htmlFor="message">Subject</label>
+        <textarea name="message" id="message"></textarea>
+      </div>
+
+      <Button text="See My Work" />
+    </form>
+  );
 };
 
 export default ContactForm;
--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,19 @@
 import "./Contact.scss";
 import ContactForm from "../../components/ContactForm/ContactForm";
-
+import SocialMediaIcons from "../../components/SocialMediaIcons/SocialMediaIcons";
 const Contact = () => {
   return (
     <div className="contact" id="contact">
-      <ContactForm />
+      <h2>Get In Touch</h2>
+      <div className="contact__content">
+        <ContactForm />
+
+        <div className="contact__details">
+          <a href="mailto:elliemae@email.com">elliemae@email.com</a>
+          <a href="tel:123123123">+ 123 123 123</a>
+          <SocialMediaIcons />
+        </div>
+      </div>
     </div>
   );
 };
